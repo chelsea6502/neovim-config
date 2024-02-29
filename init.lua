07f28949892b6778f329731629559b083f48c8ec
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-02-29 21:04:32 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -367,7 +367,6 @@ require("lazy").setup({
 	},
 	{ "williamboman/mason.nvim" },
 	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-	{ "neovim/nvim-lspconfig" },
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/nvim-cmp" },
 	{ "L3MON4D3/LuaSnip" },
