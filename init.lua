704891e6079dfb2ed2ff95db2ede84e8170215a7
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-02-29 22:29:23 +1100

    Autocomplete tabbing

--- init.lua
+++ init.lua
@@ -148,6 +148,16 @@ require("lazy").setup({
 				lsp_zero.default_keymaps({ buffer = bufnr })
 			end)
 
+			local cmp = require("cmp")
+			local cmp_action = lsp_zero.cmp_action()
+
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp_action.tab_complete(),
+					["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
+				}),
+			})
+
 			require("mason").setup({})
 			require("mason-lspconfig").setup({
 				ensure_installed = {},
