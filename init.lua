3384b7b06e5d32d24bcbbcc0f94e821c3080b9c8
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-02-29 22:38:12 +1100

    supertab

--- init.lua
+++ init.lua
@@ -153,8 +153,8 @@ require("lazy").setup({
 
 			cmp.setup({
 				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp_action.tab_complete(),
-					["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
+					["<Tab>"] = cmp_action.luasnip_supertab(),
+					["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
 				}),
 			})
