6aba2498106d30a3c42a61ef6678064ca833bf32
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-03-12 14:00:30 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -278,11 +278,11 @@ require("lazy").setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.prettierd,
-					null_ls.builtins.code_actions.gitsigns,
 					require("none-ls.diagnostics.eslint_d"),
 					require("none-ls.code_actions.eslint_d"),
 				},
 				on_attach = function(client, bufnr)
+					-- Format on save
 					if client.supports_method("textDocument/formatting") then
 						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 						vim.api.nvim_create_autocmd("BufWritePre", {
@@ -328,6 +328,7 @@ require("lazy").setup({
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {
 			settings = {
+				complete_function_calls = true,
 				expose_as_code_action = "all",
 				tsserver_file_preferences = {
 					includeInlayParameterNameHints = "literals",
